686d8d5e9edec3b7d010b8ace8a837e67b2c14d3
Julius Frost <juliustfrost@gmail.com>
2021-07-18 19:45:11 -0400

    first test

---
+++ tests/test_wrap.py
@@ -0,0 +1,41 @@
+import gym
+import pytest
+from minerl.herobraine.envs import BASIC_ENV_SPECS, COMPETITION_ENV_SPECS, BASALT_COMPETITION_ENV_SPECS
+
+from minerl_wrappers import wrap
+
+BASIC_IDS = [env_spec.name for env_spec in BASIC_ENV_SPECS]
+DIAMOND_COMPETITION_IDS = [env_spec.name for env_spec in COMPETITION_ENV_SPECS]
+BASALT_COMPETITION_IDS = [env_spec.name for env_spec in BASALT_COMPETITION_ENV_SPECS]
+
+
+def create_and_wrap(gym_id, config_file=None, **kwargs):
+    env = gym.make(gym_id)
+    env = wrap(env, config_file=config_file, **kwargs)
+    return env
+
+
+def reset_and_sample_episode(env, max_steps=None):
+    history = []
+    obs = env.reset()
+    done = False
+    steps = 0
+    while not done:
+        if max_steps is not None and steps >= max_steps:
+            break
+        action = env.action_space.sample()
+        new_obs, reward, done, info = env.step(action)
+        history.append([obs, new_obs, reward, done, info])
+        obs = new_obs
+        steps += 1
+    return history
+
+
+def build_and_run_step(gym_id, config_file=None, **kwargs):
+    env = create_and_wrap(gym_id, config_file=config_file, **kwargs)
+    reset_and_sample_episode(env, 1)
+
+
+@pytest.mark.parametrize("gym_id", DIAMOND_COMPETITION_IDS)
+def test_diamond_competition_envs(gym_id):
+    build_and_run_step(gym_id, pfrl=True)
