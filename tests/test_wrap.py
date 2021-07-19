75cb042a829099f31b1b0883f2da5c1c4d61004b
Julius <juliustfrost@gmail.com>
2021-07-19 00:28:33 -0400

    pytest pfrl 2020 wrappers

--- tests/test_wrap.py
+++ tests/test_wrap.py
@@ -1,4 +1,6 @@
 import logging
+import time
+
 import gym
 import pytest
 from minerl.herobraine.envs import (
@@ -48,6 +50,7 @@ def build_and_run_step(gym_id, config_file=None, **kwargs):
     reset_and_sample_episode(env, 1)
     logging.debug("Closing MineRL environment...")
     env.close()
+    time.sleep(10)  # let processes clean up
 
 
 @pytest.mark.parametrize("gym_id", DIAMOND_COMPETITION_IDS)
@@ -58,52 +61,3 @@ def test_diamond_competition_envs(gym_id):
         gym_id, pfrl_2020=True, pfrl_2020_config={"action_choices": means}
     )
     logging.debug("Finished test!")
-
-
-def test_pfrl_2020_wrappers():
-    env = gym.make("MineRLObtainDiamondDenseVectorObf-v0")
-    means = load_means()
-    # test default wrapper
-    wrapped_env = wrap(env)
-    reset_and_sample_episode(wrapped_env, 4)
-    # test preset action choices
-    config = {
-        "pfrl_2020": True,
-        "pfrl_2020_config": {
-            "action_choices": means,
-        },
-    }
-    wrapped_env = wrap(env, **config)
-    reset_and_sample_episode(wrapped_env, 4)
-    # test frame_skip and frame_stack
-    config = {
-        "pfrl_2020": True,
-        "pfrl_2020_config": {
-            "action_choices": means,
-            "frame_skip": 4,
-            "frame_stack": 4,
-        },
-    }
-    wrapped_env = wrap(env, **config)
-    reset_and_sample_episode(wrapped_env, 4)
-    # test gray_scale
-    config = {
-        "pfrl_2020": True,
-        "pfrl_2020_config": {
-            "action_choices": means,
-            "gray_scale": True,
-        },
-    }
-    wrapped_env = wrap(env, **config)
-    reset_and_sample_episode(wrapped_env, 4)
-    # test random_action
-    config = {
-        "pfrl_2020": True,
-        "pfrl_2020_config": {
-            "action_choices": means,
-            "random_action": True,
-            "eval_epsilon": 1,
-        },
-    }
-    wrapped_env = wrap(env, **config)
-    reset_and_sample_episode(wrapped_env, 4)
