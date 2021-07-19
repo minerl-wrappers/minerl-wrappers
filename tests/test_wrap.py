329ecdea9df77116148081b7d3286c402a06e21f
Julius <juliustfrost@gmail.com>
2021-07-18 22:06:16 -0400

    logging tests

--- tests/test_wrap.py
+++ tests/test_wrap.py
@@ -1,3 +1,4 @@
+import logging
 import gym
 import pytest
 from minerl.herobraine.envs import (
@@ -7,20 +8,26 @@ from minerl.herobraine.envs import (
 )
 
 from minerl_wrappers import wrap
+from tests.kmeans import load_means
 
 BASIC_IDS = [env_spec.name for env_spec in BASIC_ENV_SPECS]
 DIAMOND_COMPETITION_IDS = [env_spec.name for env_spec in COMPETITION_ENV_SPECS]
 BASALT_COMPETITION_IDS = [env_spec.name for env_spec in BASALT_COMPETITION_ENV_SPECS]
 
+logging.basicConfig(level=logging.DEBUG)
+
 
 def create_and_wrap(gym_id, config_file=None, **kwargs):
+    logging.debug("Creating MineRL environment...")
     env = gym.make(gym_id)
+    logging.debug("Wrapping MineRL environment...")
     env = wrap(env, config_file=config_file, **kwargs)
     return env
 
 
 def reset_and_sample_episode(env, max_steps=None):
     history = []
+    logging.debug("Resetting MineRL environment...")
     obs = env.reset()
     done = False
     steps = 0
@@ -32,14 +39,20 @@ def reset_and_sample_episode(env, max_steps=None):
         history.append([obs, new_obs, reward, done, info])
         obs = new_obs
         steps += 1
+        logging.debug(f"Environment loop step {steps} completed")
     return history
 
 
 def build_and_run_step(gym_id, config_file=None, **kwargs):
     env = create_and_wrap(gym_id, config_file=config_file, **kwargs)
     reset_and_sample_episode(env, 1)
+    logging.debug("Closing MineRL environment...")
+    env.close()
 
 
 @pytest.mark.parametrize("gym_id", DIAMOND_COMPETITION_IDS)
 def test_diamond_competition_envs(gym_id):
-    build_and_run_step(gym_id, pfrl=True)
+    logging.debug("Loading kmeans")
+    means = load_means()
+    build_and_run_step(gym_id, pfrl=True, pfrl_config={"action_choices": means})
+    logging.debug("Finished test!")
